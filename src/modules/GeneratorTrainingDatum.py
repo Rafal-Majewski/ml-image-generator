3c315a4f2d08877fb5bff6a9c998d413ac99e8cf
Rafał Majewski <goodheropl@gmail.com>
2022-06-14 14:54:43 +0200

    generator training datum

---
+++ src/modules/GeneratorTrainingDatum.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+class GeneratorTrainingDatum:
+	def __init__(
+		self,
+		*,
+		discriminations: list[float],
+		noiseSize: int,
+	) -> None:
+		self._discriminations = discriminations
+		self._noise = np.random.randn(noiseSize)
+
+	@property
+	def discriminations(self) -> list[float]:
+		return self._discriminations
+
+	@property
+	def noise(self) -> np.ndarray:
+		return self._noise
+
+	def __str__(self) -> str:
+		return f"GeneratorTrainingDatum(discriminations={self._discriminations}, noise={self._noise})"
