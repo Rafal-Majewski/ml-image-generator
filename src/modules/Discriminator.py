f3a238ed78a383c6b490a50993a59a407bb80463
Rafał Majewski <goodheropl@gmail.com>
2022-06-14 14:37:53 +0200

    reformat

--- src/modules/Discriminator.py
+++ src/modules/Discriminator.py
@@ -19,11 +19,12 @@ class Discriminator:
 		self._imageSize = imageSize
 
 	def imageToNumbers(self, image: PILImage) -> np.ndarray:
-		return np.array(image.convert("RGB").resize(self._imageSize).getdata()).flatten()
+		return np.array(image.convert("RGB").resize(self._imageSize).getdata()).flatten() / 255
 
 	def discriminate(self, image: PILImage) -> list[float]:
 		discriminations: list[float] = self._model(
 			np.array([self.imageToNumbers(image)]),
+			training=False,
 		).numpy()[0]
 		return discriminations
 
@@ -34,6 +35,7 @@ class Discriminator:
 	) -> None:
 		shuffedData = list(data)
 		random.shuffle(shuffedData)
+
 		x: np.ndarray = np.array([self.imageToNumbers(datum.image) for datum in shuffedData])
 		y: np.ndarray = np.array([datum.discriminations for datum in shuffedData])
 		self._model.fit(
